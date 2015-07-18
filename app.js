db39ea30b7805227282910a1018827f57346d110
Yonatan Schultz <yonatan.schultz@gmail.com>
2015-07-18 16:45:47 -0700

    updated app.js to work with openshift

--- app.js
+++ app.js
@@ -59,10 +59,17 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
+/*
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+});
+*/
+var server = app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
